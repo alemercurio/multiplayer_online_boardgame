3917273dc24b2e243ed1186f201d15d68c613ddd
alemercurio <mercurioalessandro99@gmail.com>
2021-04-05 23:42:03 +0200

    fix: add getCopy() in ColorPack

--- src/main/java/it/polimi/ingsw/ColorPack.java
+++ src/main/java/it/polimi/ingsw/ColorPack.java
@@ -25,6 +25,10 @@ public class ColorPack {
 
     private HashMap<Pair, Integer> cardSet;
 
+    public ColorPack() {
+        cardSet = new HashMap<Pair, Integer>();
+    }
+
     /**
      * Add a DevelopmentCard with a certain Color and level to the ColorPack.
      * @param color the Color of the card to add to the pack.
@@ -32,14 +36,15 @@ public class ColorPack {
      */
     public void addColor(Color color, int level) {
         Pair cardToAdd = new Pair(color, level);
+        int number;
         if(cardSet.get(cardToAdd)!=null) {
-            int number = cardSet.get(cardToAdd);
+            number = cardSet.get(cardToAdd);
             number = number + 1;
         }
         else {
             number = 1;
-            cardSet.put(cardToAdd, number);
         }
+        cardSet.put(cardToAdd, number);
     }
 
     /**
@@ -48,16 +53,28 @@ public class ColorPack {
      * @return a boolean that answer the question.
      */
     public boolean testRequirements(ColorPack required) {
-        boolean available = True;
+        boolean available = true;
         for(Pair pair : required.cardSet.keySet()) {
             Integer num = this.cardSet.get(pair);
             if(num==null) {
-                available = False;
+                available = false;
             }
             else if(required.cardSet.get(pair) > num) {
-                available = False;
+                available = false;
             }
         }
         return available;
+    }
+
+    /**
+     * Return a copy of the ColorPack in order to get the same set of DevelopmentCards.
+     * @return a new ColorPack with copied inner representation.
+     */
+    public ColorPack getCopy() {
+        ColorPack copy = new ColorPack();
+        for(Pair pair : this.cardSet.keySet()) {
+            copy.cardSet.put(pair, this.cardSet.get(pair));
+        }
+        return copy;
     }
 }
