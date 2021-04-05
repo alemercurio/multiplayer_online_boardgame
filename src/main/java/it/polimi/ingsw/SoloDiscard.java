3752999be530f5c178f31169aad6d6b26fb26974
Patrick <Patrickorelien.niantcho@mail.polimi.it>
2021-04-06 01:15:34 +0200

    fix: update Single Player and add javadoc

--- src/main/java/it/polimi/ingsw/SoloDiscard.java
+++ src/main/java/it/polimi/ingsw/SoloDiscard.java
@@ -1,15 +1,29 @@
 package it.polimi.ingsw;
 
+import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author patrick Niantcho
+ * @author patrick.niantcho@gmail.com
+ */
 public class SoloDiscard implements SoloAction {
-
-    public SoloDiscard(Map<Color, Integer> toDiscard) {
-        this.toDiscard = toDiscard;
-    }
 
     private Map<Color, Integer> toDiscard;
 
+    /**
+     *
+     * @param toDiscard represent the number of a specified card to discard
+     */
+    public SoloDiscard(HashMap<Color, Integer> toDiscard) {
+        this.toDiscard = toDiscard;
+    }
+
+    /**
+     *
+     * @param player represent LorenzoIlMagnifico
+     * @return specify that after apply the action, to action desk should not be shuffled
+     */
     @Override
     public boolean Apply(LorenzoIlMagnifico player) {
        for ( Map.Entry<Color, Integer> pair : toDiscard.entrySet() )
@@ -17,6 +31,10 @@ public class SoloDiscard implements SoloAction {
        return false;
     }
 
+    /**
+     *
+     * @return the effect of this action on the MarketBoard
+     */
     public Map<Color, Integer> getToDiscard() {
         return toDiscard;
     }
