c2b19683ac4ffedc7ce1ab5144cebf793515ed25
FrancescoTosini <79970907+FrancescoTosini@users.noreply.github.com>
2021-04-11 02:23:18 +0200

    refactor: Storage and StockPower

--- src/main/java/it/polimi/ingsw/StockPower.java
+++ src/main/java/it/polimi/ingsw/StockPower.java
@@ -7,11 +7,6 @@ package it.polimi.ingsw;
 public class StockPower implements Power {
     private final int size;
     private final Resource type;
-    private int used;
-
-    // TODO: la dimensione potrebbe essere modificata a posteriori;
-    //  ciò può consentire di utilizzare un solo StockPower per modellarne molti...
-    //  puoi implementare un metodo merge()...
 
     /**
      * Constructs a StockPower with the ability to store at most the given amount
@@ -22,7 +17,6 @@ public class StockPower implements Power {
     public StockPower(int size, Resource type) {
         this.size = size;
         this.type = type;
-        this.used = 0;
     }
 
     /**
@@ -43,57 +37,9 @@ public class StockPower implements Power {
     }
 
     /**
-     * Returns the pack of Resources stored in the depot.
-     * @return the ResourcePack corresponding to the stocked Resources.
+     * Returns the maximum amount of resources containable in the
+     * additional depot granted by the activation of the current power.
+     * @return the size of the stock given by the current power.
      */
-    public ResourcePack getResources() {
-        return (new ResourcePack()).add(type, used);
-    }
-
-    /**
-     * Returns the amount of resources stored in the current depot.
-     * The type of resources is left implicit.
-     * @return the amount of stored resources.
-     */
-    public int getUsed() {
-        return this.used;
-    }
-
-    /**
-     * Stores the maximum amount of Resources of the right type,
-     * from the given pack, into the depot;
-     * the same amount of resources is consumed from the given pack.
-     * @param pack the ResourcePack containing the resources to store into the depot.
-     */
-    public void add(ResourcePack pack) {
-        if (this.used < this.size) {
-            // Adds all the available resources of the right type.
-            this.used = this.used + pack.flush(this.type);
-            // Overflow is then collected
-            if (this.used > this.size) {
-                pack.add(this.type, this.used - this.size);
-                this.used = this.size;
-            }
-        }
-    }
-
-    /**
-     * If there are enough Resources, consume the amount specified in the input ResourcePack;
-     * partial consuming is allowed.
-     * @param pack the ResourcePack to consume from the depot.
-     * @return the ResourcePack of non-consumable resources.
-     */
-    public ResourcePack consume(ResourcePack pack) {
-        ResourcePack leftToConsume = pack.getCopy();
-        if (this.used > 0) {
-            // Consumes all the required resources of the right type.
-            this.used = this.used - leftToConsume.flush(this.type);
-            // Missing resources returns as an unfulfilled requirement
-            if (this.used < 0) {
-                leftToConsume.add(this.type, (-this.used));
-                this.used = 0;
-            }
-        }
-        return leftToConsume;
-    }
+    public int getLimit() { return this.size; }
 }
