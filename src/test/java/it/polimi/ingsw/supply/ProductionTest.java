f9bd23e888a1ad94075d147400fbba512eea6e20
alemercurio <mercurioalessandro99@gmail.com>
2021-04-22 11:57:37 +0200

    test: Production

--- src/test/java/it/polimi/ingsw/supply/ProductionTest.java
+++ src/test/java/it/polimi/ingsw/supply/ProductionTest.java
@@ -3,30 +3,46 @@ package it.polimi.ingsw.supply;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertTrue;
 
 public class ProductionTest {
 
     @Test
-    public void getRequired() {
-        ResourcePack rp1 = new ResourcePack(1,2);
-        ResourcePack rp2 = new ResourcePack(4,0,7);
-        Production p = new Production(rp1,rp2);
+    public void testGetRequired() {
+        ResourcePack input = new ResourcePack(1,2); //1 coin, 2 stones
+        ResourcePack output = new ResourcePack(0,0,7); //7 servants
+        Production p = new Production(input,output);
 
-        assertEquals(p.getRequired(),rp1);
-        assertEquals(p.produce(),rp2);
+        assertEquals(p.getRequired(),input);
+    }
+
+    @Test
+    public void testProduce() {
+        ResourcePack input = new ResourcePack(1,2); //1 coin, 2 stones
+        ResourcePack output = new ResourcePack(0,0,7); //7 servants
+        Production p = new Production(input,output);
+
+        assertEquals(p.produce(),output);
     }
 
     @Test
     public void testEquals() {
-        String str = "Casa";
-        ResourcePack rp1 = new ResourcePack(1,2);
-        ResourcePack rp2 = new ResourcePack(4,0,7);
-        Production p = new Production(rp1,rp2);
-        Production anotherP = new Production(rp1,rp2);
+        String str = "Not a Production";
+        ResourcePack input = new ResourcePack(1,2); //1 coin, 2 stones
+        ResourcePack output = new ResourcePack(4,0,7); //4 coins, 7 servants
+        ResourcePack inputWithFaith = new ResourcePack(1,2,0,0,1); //1 coin, 2 stones, 1 faithpoint
+        ResourcePack outputWithFaith = new ResourcePack(4,0,7,0,2); //4 coins, 7 servants, 2 faithpoints
+        Production p = new Production(input,output);
+        Production anotherP = new Production(input,output);
+        Production notP = new Production(output,input);
+        Production pWithFaith = new Production(inputWithFaith, output);
+        Production notPWithFaith = new Production(input, outputWithFaith);
 
-        assertFalse(p.equals(str));
-        assertTrue(p.equals(p));
-        assertTrue(p.equals(anotherP));
+        assertNotEquals(p, null);
+        assertNotEquals(p, str);
+        assertEquals(p, p);
+        assertEquals(p, anotherP);
+        assertNotEquals(p, notP);
+        assertEquals(p, pWithFaith);
+        assertNotEquals(p, notPWithFaith);
     }
 }
