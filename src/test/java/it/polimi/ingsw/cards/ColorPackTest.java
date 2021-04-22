8ef901452fff025c26b025b3c2aa409209fa2754
Patrick <Patrickorelien.niantcho@mail.polimi.it>
2021-04-22 17:55:39 +0200

    test: add initial tests for LeaderStack and ColorPack

---
+++ src/test/java/it/polimi/ingsw/cards/ColorPackTest.java
@@ -0,0 +1,94 @@
+package it.polimi.ingsw.cards;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ColorPackTest {
+
+    @Test
+    public void addColor() {
+
+        ColorPack colorPack = new ColorPack();
+        assertEquals(colorPack.toString(), "{\n}");
+
+        colorPack.addColor(Color.GREEN, 1);
+        assertEquals(colorPack.toString(), "{\n\t{GREEN,1}: 1\n}");
+
+        colorPack.addColor(Color.GREEN, 1);
+        assertEquals(colorPack.toString(), "{\n\t{GREEN,1}: 2\n}");
+
+        colorPack.addColor(Color.GREEN, 2);
+        assertEquals(colorPack.toString(), "{\n\t{GREEN,1}: 2\n\t{GREEN,2}: 1\n}");
+
+        colorPack.addColor(Color.BLUE, 1);
+        assertEquals(colorPack.toString(), "{\n\t{BLUE,1}: 1\n\t{GREEN,1}: 2\n\t{GREEN,2}: 1\n}");
+
+        colorPack.addColor(Color.BLUE, 1);
+        assertEquals(colorPack.toString(), "{\n\t{BLUE,1}: 2\n\t{GREEN,1}: 2\n\t{GREEN,2}: 1\n}");
+
+        colorPack.addColor(Color.BLUE, 2);
+        assertEquals(colorPack.toString(), "{\n\t{BLUE,1}: 2\n\t{GREEN,1}: 2\n\t{GREEN,2}: 1\n\t{BLUE,2}: 1\n}");
+
+        colorPack.addColor(Color.PURPLE, 3);
+        assertEquals(colorPack.toString(), "{\n\t{BLUE,1}: 2\n\t{PURPLE,3}: 1\n\t{GREEN,1}: 2\n\t{GREEN,2}: 1\n\t{BLUE,2}: 1\n}");
+
+
+    }
+
+    @Test
+    public void testRequirements() {
+
+        ColorPack colorPack = new ColorPack();
+        ColorPack required = new ColorPack();
+        assertTrue(colorPack.testRequirements(required));
+
+        required.addColor(Color.BLUE, 2);
+        assertFalse(colorPack.testRequirements(required));
+
+        colorPack.addColor(Color.GREEN, 1);
+        colorPack.addColor(Color.GREEN, 1);
+        colorPack.addColor(Color.GREEN, 2);
+        colorPack.addColor(Color.BLUE, 1);
+        colorPack.addColor(Color.BLUE, 1);
+        colorPack.addColor(Color.BLUE, 2);
+        colorPack.addColor(Color.PURPLE, 3);
+
+        //check if the answer is true despite the fact that colorPack has a level 3 PURPLE card and required a level 2 PURPLE card.
+        required.addColor(Color.PURPLE, 2);
+        assertTrue(colorPack.testRequirements(required));
+
+        required.addColor(Color.GREEN, 1);
+        required.addColor(Color.GREEN, 1);
+        required.addColor(Color.GREEN, 2);
+        assertTrue(colorPack.testRequirements(required));
+
+        required.addColor(Color.BLUE,3);
+        assertFalse(colorPack.testRequirements(required));
+
+        colorPack.addColor(Color.BLUE,3);
+        assertTrue(colorPack.testRequirements(required));
+
+        required.addColor(Color.YELLOW,2);
+        assertFalse(colorPack.testRequirements(required));
+    }
+
+    @Test
+    public void getCopy() {
+
+        ColorPack colorPack = new ColorPack();
+        colorPack.addColor(Color.GREEN, 1);
+        colorPack.addColor(Color.GREEN, 1);
+        colorPack.addColor(Color.GREEN, 2);
+        colorPack.addColor(Color.BLUE, 1);
+        colorPack.addColor(Color.BLUE, 1);
+        colorPack.addColor(Color.BLUE, 2);
+        colorPack.addColor(Color.PURPLE, 3);
+
+        ColorPack copy = colorPack.getCopy();
+        assertEquals(colorPack, copy);
+
+        copy.addColor(Color.BLUE,1);
+        assertNotEquals(colorPack,copy);
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/cards/LeaderStackTest.java
@@ -0,0 +1,475 @@
+package it.polimi.ingsw.cards;
+
+import it.polimi.ingsw.PlayerBoard;
+import it.polimi.ingsw.faith.FaithTrack;
+import it.polimi.ingsw.supply.MarketBoard;
+import it.polimi.ingsw.supply.Production;
+import it.polimi.ingsw.supply.Resource;
+import it.polimi.ingsw.supply.ResourcePack;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LeaderStackTest {
+
+    @Test
+    public void addLeaders() {
+        LeaderStack stack = new LeaderStack();
+
+        ResourcePack rp1_cost = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp2_cost = new ResourcePack(1,1,1,0,0);
+        ResourcePack rp3_cost = new ResourcePack(2,2,0,0,0);
+
+        ColorPack colorPack1 = new ColorPack();
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 2);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 2);
+        colorPack1.addColor(Color.PURPLE, 3);
+
+        ColorPack colorPack2 = new ColorPack();
+        colorPack2.addColor(Color.PURPLE, 1);
+        colorPack2.addColor(Color.GREEN, 1);
+
+        ColorPack colorPack3 = new ColorPack();
+        colorPack3.addColor(Color.PURPLE, 1);
+        colorPack3.addColor(Color.BLUE, 3);
+
+        ResourcePack rp1_input = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp1_output = new ResourcePack(1,1,0,4,0);
+        Production p1 = new Production(rp1_input, rp1_output);
+
+        Power power1 = new ProductionPower(p1);
+        Power power2 = new StockPower(2, Resource.SERVANT);
+        Power power3 = new VoidPower(Resource.COIN);
+
+        LeaderCard leader1 = new LeaderCard(2, rp1_cost, colorPack1, power1);
+        LeaderCard leader2 = new LeaderCard(3, rp2_cost, colorPack2, power2);
+        LeaderCard leader3 = new LeaderCard(1, rp3_cost, colorPack3, power3);
+
+        List<LeaderCard> list = new LinkedList<>();
+        list.add(leader1);
+        list.add(leader2);
+        list.add(leader3);
+
+        stack.addLeaders(list);
+        assertEquals(stack.getInactiveLeader(), list);
+
+        LeaderCard leader4 = new LeaderCard(5, rp1_cost, colorPack3, power3);
+        list.add(leader4);
+        assertNotEquals(stack.getInactiveLeader(), list);
+
+    }
+
+    @Test
+    public void activate() {
+        MarketBoard marketBoard = new MarketBoard();
+        FaithTrack faithTrack = new FaithTrack (null, null, 1,null);
+        PlayerBoard playerBoard = new PlayerBoard(marketBoard, faithTrack);
+
+
+        LeaderStack stack = new LeaderStack();
+
+        ResourcePack rp1_cost = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp2_cost = new ResourcePack(1,1,1,0,0);
+        ResourcePack rp3_cost = new ResourcePack(2,2,0,0,0);
+
+        ColorPack colorPack1 = new ColorPack();
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 2);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 2);
+        colorPack1.addColor(Color.PURPLE, 3);
+
+        ColorPack colorPack2 = new ColorPack();
+        colorPack2.addColor(Color.PURPLE, 1);
+        colorPack2.addColor(Color.GREEN, 1);
+
+        ColorPack colorPack3 = new ColorPack();
+        colorPack3.addColor(Color.PURPLE, 1);
+        colorPack3.addColor(Color.BLUE, 3);
+
+        ResourcePack rp1_input = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp1_output = new ResourcePack(1,1,0,4,0);
+        Production p1 = new Production(rp1_input, rp1_output);
+
+        Power power1 = new ProductionPower(p1);
+        Power power2 = new StockPower(2, Resource.SERVANT);
+        Power power3 = new VoidPower(Resource.COIN);
+
+        LeaderCard leader1 = new LeaderCard(2, rp1_cost, colorPack1, power1);
+        LeaderCard leader2 = new LeaderCard(3, rp2_cost, colorPack2, power2);
+        LeaderCard leader3 = new LeaderCard(1, rp3_cost, colorPack3, power3);
+
+        List<LeaderCard> list = new LinkedList<>();
+        list.add(leader1);
+        list.add(leader2);
+        list.add(leader3);
+
+        stack.addLeaders(list);
+
+
+        // check with an invalid index
+        stack.activate(3, playerBoard);
+        assertEquals(stack.getActiveLeader().size(),0 );
+
+        stack.activate(0, playerBoard);
+        assertEquals(stack.getActiveLeader().size(), 1);
+        assertEquals(stack.getActiveLeader(0), leader1);
+        assertFalse(stack.getInactiveLeader().contains(leader1));
+        assertEquals(stack.getInactiveLeader().size(), 2);
+
+        stack.activate(0, playerBoard);
+        assertEquals(stack.getActiveLeader().size(), 2);
+        assertEquals(stack.getActiveLeader(1), leader2);
+        assertFalse(stack.getInactiveLeader().contains(leader2));
+        assertEquals(stack.getInactiveLeader().size(), 1);
+
+        stack.activate(0, playerBoard);
+        assertEquals(stack.getActiveLeader().size(), 3);
+        assertEquals(stack.getActiveLeader(2), leader3);
+        assertFalse(stack.getInactiveLeader().contains(leader3));
+        assertEquals(stack.getInactiveLeader().size(), 0);
+    }
+
+    @Test
+    public void discard() {
+
+        LeaderStack stack = new LeaderStack();
+
+        ResourcePack rp1_cost = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp2_cost = new ResourcePack(1,1,1,0,0);
+        ResourcePack rp3_cost = new ResourcePack(2,2,0,0,0);
+
+        ColorPack colorPack1 = new ColorPack();
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 2);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 2);
+        colorPack1.addColor(Color.PURPLE, 3);
+
+        ColorPack colorPack2 = new ColorPack();
+        colorPack2.addColor(Color.PURPLE, 1);
+        colorPack2.addColor(Color.GREEN, 1);
+
+        ColorPack colorPack3 = new ColorPack();
+        colorPack3.addColor(Color.PURPLE, 1);
+        colorPack3.addColor(Color.BLUE, 3);
+
+        ResourcePack rp1_input = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp1_output = new ResourcePack(1,1,0,4,0);
+        Production p1 = new Production(rp1_input, rp1_output);
+
+        Power power1 = new ProductionPower(p1);
+        Power power2 = new StockPower(2, Resource.SERVANT);
+        Power power3 = new VoidPower(Resource.COIN);
+
+        LeaderCard leader1 = new LeaderCard(2, rp1_cost, colorPack1, power1);
+        LeaderCard leader2 = new LeaderCard(3, rp2_cost, colorPack2, power2);
+        LeaderCard leader3 = new LeaderCard(1, rp3_cost, colorPack3, power3);
+
+        List<LeaderCard> list = new LinkedList<>();
+        list.add(leader1);
+        list.add(leader2);
+        list.add(leader3);
+
+        stack.addLeaders(list);
+
+        stack.discard(3);
+        assertEquals(stack.getInactiveLeader().size(), 3);
+
+        stack.discard(1);
+        assertFalse(stack.getInactiveLeader().contains(leader2));
+        assertEquals(stack.getInactiveLeader().size(), 2);
+
+        stack.discard(1);
+        assertFalse(stack.getInactiveLeader().contains(leader3));
+        assertEquals(stack.getInactiveLeader().size(), 1);
+
+        stack.discard(0);
+        assertFalse(stack.getInactiveLeader().contains(leader1));
+        assertEquals(stack.getInactiveLeader().size(), 0);
+    }
+
+    @Test
+    public void getActiveLeader() {
+
+        MarketBoard marketBoard = new MarketBoard();
+        FaithTrack faithTrack = new FaithTrack (null, null, 1,null);
+        PlayerBoard playerBoard = new PlayerBoard(marketBoard, faithTrack);
+
+        LeaderStack stack = new LeaderStack();
+
+        ResourcePack rp1_cost = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp2_cost = new ResourcePack(1,1,1,0,0);
+        ResourcePack rp3_cost = new ResourcePack(2,2,0,0,0);
+
+        ColorPack colorPack1 = new ColorPack();
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 2);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 2);
+        colorPack1.addColor(Color.PURPLE, 3);
+
+        ColorPack colorPack2 = new ColorPack();
+        colorPack2.addColor(Color.PURPLE, 1);
+        colorPack2.addColor(Color.GREEN, 1);
+
+        ColorPack colorPack3 = new ColorPack();
+        colorPack3.addColor(Color.PURPLE, 1);
+        colorPack3.addColor(Color.BLUE, 3);
+
+        ResourcePack rp1_input = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp1_output = new ResourcePack(1,1,0,4,0);
+        Production p1 = new Production(rp1_input, rp1_output);
+
+        Power power1 = new ProductionPower(p1);
+        Power power2 = new StockPower(2, Resource.SERVANT);
+        Power power3 = new VoidPower(Resource.COIN);
+
+        LeaderCard leader1 = new LeaderCard(2, rp1_cost, colorPack1, power1);
+        LeaderCard leader2 = new LeaderCard(3, rp2_cost, colorPack2, power2);
+        LeaderCard leader3 = new LeaderCard(1, rp3_cost, colorPack3, power3);
+
+        List<LeaderCard> list = new LinkedList<>();
+        list.add(leader1);
+        list.add(leader2);
+        list.add(leader3);
+
+        stack.addLeaders(list);
+
+        assertNull(stack.getActiveLeader(0));
+
+        int index = 0;
+        for (int i = 0; i < 3; i++)
+            stack.activate(index,playerBoard);
+
+        assertEquals(stack.getActiveLeader(0), leader1);
+        assertEquals(stack.getActiveLeader(1), leader2);
+        assertEquals(stack.getActiveLeader(2), leader3);
+    }
+
+    @Test
+    public void testGetActiveLeader() {
+        MarketBoard marketBoard = new MarketBoard();
+        FaithTrack faithTrack = new FaithTrack (null, null, 1,null);
+        PlayerBoard playerBoard = new PlayerBoard(marketBoard, faithTrack);
+
+        LeaderStack stack = new LeaderStack();
+
+        ResourcePack rp1_cost = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp2_cost = new ResourcePack(1,1,1,0,0);
+        ResourcePack rp3_cost = new ResourcePack(2,2,0,0,0);
+
+        ColorPack colorPack1 = new ColorPack();
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 2);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 2);
+        colorPack1.addColor(Color.PURPLE, 3);
+
+        ColorPack colorPack2 = new ColorPack();
+        colorPack2.addColor(Color.PURPLE, 1);
+        colorPack2.addColor(Color.GREEN, 1);
+
+        ColorPack colorPack3 = new ColorPack();
+        colorPack3.addColor(Color.PURPLE, 1);
+        colorPack3.addColor(Color.BLUE, 3);
+
+        ResourcePack rp1_input = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp1_output = new ResourcePack(1,1,0,4,0);
+        Production p1 = new Production(rp1_input, rp1_output);
+
+        Power power1 = new ProductionPower(p1);
+        Power power2 = new StockPower(2, Resource.SERVANT);
+        Power power3 = new VoidPower(Resource.COIN);
+
+        LeaderCard leader1 = new LeaderCard(2, rp1_cost, colorPack1, power1);
+        LeaderCard leader2 = new LeaderCard(3, rp2_cost, colorPack2, power2);
+        LeaderCard leader3 = new LeaderCard(1, rp3_cost, colorPack3, power3);
+
+        List<LeaderCard> list = new LinkedList<>();
+        list.add(leader1);
+        list.add(leader2);
+        list.add(leader3);
+
+        stack.addLeaders(list);
+
+        assertEquals(stack.getActiveLeader().size(), 0);
+
+        int index = 0;
+        for (int i = 0; i < 3; i++)
+            stack.activate(index,playerBoard);
+
+        assertEquals(stack.getActiveLeader(), list);
+    }
+
+    @Test
+    public void getInactiveLeader() {
+
+        LeaderStack stack = new LeaderStack();
+
+        ResourcePack rp1_cost = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp2_cost = new ResourcePack(1,1,1,0,0);
+        ResourcePack rp3_cost = new ResourcePack(2,2,0,0,0);
+
+        ColorPack colorPack1 = new ColorPack();
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 2);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 2);
+        colorPack1.addColor(Color.PURPLE, 3);
+
+        ColorPack colorPack2 = new ColorPack();
+        colorPack2.addColor(Color.PURPLE, 1);
+        colorPack2.addColor(Color.GREEN, 1);
+
+        ColorPack colorPack3 = new ColorPack();
+        colorPack3.addColor(Color.PURPLE, 1);
+        colorPack3.addColor(Color.BLUE, 3);
+
+        ResourcePack rp1_input = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp1_output = new ResourcePack(1,1,0,4,0);
+        Production p1 = new Production(rp1_input, rp1_output);
+
+        Power power1 = new ProductionPower(p1);
+        Power power2 = new StockPower(2, Resource.SERVANT);
+        Power power3 = new VoidPower(Resource.COIN);
+
+        LeaderCard leader1 = new LeaderCard(2, rp1_cost, colorPack1, power1);
+        LeaderCard leader2 = new LeaderCard(3, rp2_cost, colorPack2, power2);
+        LeaderCard leader3 = new LeaderCard(1, rp3_cost, colorPack3, power3);
+
+        List<LeaderCard> list = new LinkedList<>();
+        list.add(leader1);
+        list.add(leader2);
+        list.add(leader3);
+
+        stack.addLeaders(list);
+
+        assertNull(stack.getInactiveLeader(3));
+
+        assertEquals(stack.getInactiveLeader(0), leader1);
+        assertEquals(stack.getInactiveLeader(1), leader2);
+        assertEquals(stack.getInactiveLeader(2), leader3);
+    }
+
+    @Test
+    public void testGetInactiveLeader() {
+
+        LeaderStack stack = new LeaderStack();
+        assertEquals(stack.getInactiveLeader().size(), 0);
+
+        ResourcePack rp1_cost = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp2_cost = new ResourcePack(1,1,1,0,0);
+        ResourcePack rp3_cost = new ResourcePack(2,2,0,0,0);
+
+        ColorPack colorPack1 = new ColorPack();
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 2);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 2);
+        colorPack1.addColor(Color.PURPLE, 3);
+
+        ColorPack colorPack2 = new ColorPack();
+        colorPack2.addColor(Color.PURPLE, 1);
+        colorPack2.addColor(Color.GREEN, 1);
+
+        ColorPack colorPack3 = new ColorPack();
+        colorPack3.addColor(Color.PURPLE, 1);
+        colorPack3.addColor(Color.BLUE, 3);
+
+        ResourcePack rp1_input = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp1_output = new ResourcePack(1,1,0,4,0);
+        Production p1 = new Production(rp1_input, rp1_output);
+
+        Power power1 = new ProductionPower(p1);
+        Power power2 = new StockPower(2, Resource.SERVANT);
+        Power power3 = new VoidPower(Resource.COIN);
+
+        LeaderCard leader1 = new LeaderCard(2, rp1_cost, colorPack1, power1);
+        LeaderCard leader2 = new LeaderCard(3, rp2_cost, colorPack2, power2);
+        LeaderCard leader3 = new LeaderCard(1, rp3_cost, colorPack3, power3);
+
+        List<LeaderCard> list = new LinkedList<>();
+        list.add(leader1);
+        list.add(leader2);
+        list.add(leader3);
+
+        stack.addLeaders(list);
+        assertEquals(stack.getInactiveLeader(), list);
+    }
+
+    @Test
+    public void getPoints() {
+        MarketBoard marketBoard = new MarketBoard();
+        FaithTrack faithTrack = new FaithTrack (null, null, 1,null);
+        PlayerBoard playerBoard = new PlayerBoard(marketBoard, faithTrack);
+
+        LeaderStack stack = new LeaderStack();
+        assertEquals(stack.getPoints(),0);
+
+        ResourcePack rp1_cost = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp2_cost = new ResourcePack(1,1,1,0,0);
+        ResourcePack rp3_cost = new ResourcePack(2,2,0,0,0);
+
+        ColorPack colorPack1 = new ColorPack();
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 1);
+        colorPack1.addColor(Color.GREEN, 2);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 1);
+        colorPack1.addColor(Color.BLUE, 2);
+        colorPack1.addColor(Color.PURPLE, 3);
+
+        ColorPack colorPack2 = new ColorPack();
+        colorPack2.addColor(Color.PURPLE, 1);
+        colorPack2.addColor(Color.GREEN, 1);
+
+        ColorPack colorPack3 = new ColorPack();
+        colorPack3.addColor(Color.PURPLE, 1);
+        colorPack3.addColor(Color.BLUE, 3);
+
+        ResourcePack rp1_input = new ResourcePack(0,0,3,0,0);
+        ResourcePack rp1_output = new ResourcePack(1,1,0,4,0);
+        Production p1 = new Production(rp1_input, rp1_output);
+
+        Power power1 = new ProductionPower(p1);
+        Power power2 = new StockPower(2, Resource.SERVANT);
+        Power power3 = new VoidPower(Resource.COIN);
+
+        LeaderCard leader1 = new LeaderCard(2, rp1_cost, colorPack1, power1);
+        LeaderCard leader2 = new LeaderCard(3, rp2_cost, colorPack2, power2);
+        LeaderCard leader3 = new LeaderCard(1, rp3_cost, colorPack3, power3);
+
+        List<LeaderCard> list = new LinkedList<>();
+        list.add(leader1);
+        list.add(leader2);
+        list.add(leader3);
+
+        stack.addLeaders(list);
+        int index = 0;
+        for (int i = 0; i < 3; i++)
+            stack.activate(index,playerBoard);
+
+        assertEquals(stack.getPoints(),6);
+    }
+}
