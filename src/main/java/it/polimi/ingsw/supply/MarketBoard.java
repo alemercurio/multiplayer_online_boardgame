a84bab14bcd4a6267c9267537885cdff7cce2802
alemercurio <mercurioalessandro99@gmail.com>
2021-04-24 22:15:55 +0200

    style: MarketBoard

--- src/main/java/it/polimi/ingsw/supply/MarketBoard.java
+++ src/main/java/it/polimi/ingsw/supply/MarketBoard.java
@@ -6,31 +6,28 @@ import it.polimi.ingsw.cards.DevelopmentCard;
 import java.util.*;
 
 /**
- * The MarketBoard class represents both the resource's market and the
- * DevelopmentCards' one. It offers methods for gathering resources from the market
- * or to buy a DevelopmentCard.
+ * The MarketBoard class represents both the Market Tray and the Development Cards matrix of decks.
+ * It offers methods for gathering Resources as Market Marbles and to buy a Development Card.
  */
 public class MarketBoard {
-    private final ResourceMarket resourceMarket;
-    private final CardMarket cardMarket;
+    public final ResourceMarket resourceMarket;
+    public final CardMarket cardMarket;
 
     /**
-     * Represent the resource section of the MarketBoard.
+     * Represents the Market Tray with the Market Marbles, represented by the corresponding Resource.
      */
-    public static class ResourceMarket
-    {
+    public static class ResourceMarket {
         private final Resource[][] marketTray;
         private Resource remaining;
 
         /**
-         * Constructs a ResourceMarket containing the given resources in random order.
-         * if the resources provided are not enough to fill the market
-         * empty spaces are filled with VOID; if the pack has more resources than necessary
-         * the excess is ignored.
-         * @param resources the resources to initialize the market with.
+         * Constructs a ResourceMarket containing the given Resources in random order.
+         * if the Resources provided are not enough to fill the Market Tray,
+         * empty spaces are filled with VOID (white Marble);
+         * if the pack has more resources than necessary the excess is ignored.
+         * @param resources the ResourcePack to initialize the market with.
          */
-        public ResourceMarket(ResourcePack resources)
-        {
+        public ResourceMarket(ResourcePack resources) {
             this.marketTray = new Resource[3][4];
             for(int r = 0;r < 3;r++)
                 for(int c = 0;c < 4;c++)
@@ -39,38 +36,35 @@ public class MarketBoard {
         }
 
         /**
-         * Construct a standard ResourceMarket.
+         * Constructs a standard ResourceMarket.
          */
-        public ResourceMarket()
-        {
-            // Standard resources for the marketTray:
+        public ResourceMarket() {
+            // Standard Resources for the marketTray:
             //      2 coins,
             //      2 stones,
             //      2 servants,
             //      2 shields,
-            //      1 faithpoint,
+            //      1 faithpoints,
             //      4 voids
 
             this(new ResourcePack(2,2,2,2,1,4));
         }
 
         /**
-         * Gather resources from the given column; market shift is automatically handled.
+         * Gathers Resources from the given column; marbles shift is automatically handled.
          * @param column the column whose resources are desired.
-         * @return a pack of the resources previously present in the given column.
+         * @return a pack of the Resources previously present in the given column.
          */
-        public ResourcePack getColumn(int column)
-        {
+        public ResourcePack getColumn(int column) {
             ResourcePack resources = new ResourcePack();
 
-            // Adjust the index of column
+            //adjust the index of column
             if(column >= 4) column = 3;
             else if(column < 0) column = 0;
 
-            // Gathers resources and shifts them
+            //gathers resources and shifts them
             Resource tmp_resource;
-            for(int row = 2;row >= 0; row--)
-            {
+            for(int row = 2;row >= 0; row--) {
                 tmp_resource = this.marketTray[row][column];
                 resources.add(tmp_resource,1);
                 this.marketTray[row][column] = this.remaining;
@@ -81,12 +75,11 @@ public class MarketBoard {
         }
 
         /**
-         * Gather resources from the given row; market shift is automatically handled.
+         * Gathers Resources from the given row; marbles shift is automatically handled.
          * @param row the row whose resources are desired.
          * @return a pack of the resources previously present in the given row.
          */
-        public ResourcePack getRow(int row)
-        {
+        public ResourcePack getRow(int row) {
             ResourcePack resources = new ResourcePack();
 
             // Adjust the index of column
@@ -95,58 +88,49 @@ public class MarketBoard {
 
             // Gathers resources and shifts them
             Resource tmp_resource;
-            for(int column = 3;column >= 0; column--)
-            {
+            for(int column = 3;column >= 0; column--) {
                 tmp_resource = this.marketTray[row][column];
                 resources.add(tmp_resource,1);
                 this.marketTray[row][column] = this.remaining;
                 this.remaining = tmp_resource;
             }
-
             return resources;
         }
 
         @Override
-        public String toString()
-        {
-            String resMarket = "{";
-            for(int r = 0;r < 3;r++)
-            {
-                resMarket = resMarket + "\n\t";
-                for (int c = 0; c < 4; c++)
-                {
-                    resMarket = resMarket + marketTray[r][c].getAlias();
+        public String toString() {
+            StringBuilder resMarket = new StringBuilder("{");
+            for(int r = 0;r < 3;r++) {
+                resMarket.append("\n\t");
+                for (int c = 0; c < 4; c++) {
+                    resMarket.append(marketTray[r][c].getAlias());
                 }
             }
-            resMarket = resMarket + "\n}";
-            return resMarket;
+            resMarket.append("\n}");
+            return resMarket.toString();
         }
     }
 
     /**
-     * Represents the Development Card section of the MarketBoard.
+     * Represents the Development Cards section of the MarketBoard.
      */
-    public static class CardMarket
-    {
+    public static class CardMarket {
         private final Map<Color, List<LinkedList<DevelopmentCard>>> decksMap;
 
         /**
          * Constructs a CardMarket and loads DevelopmentCards into it.
          */
-        public CardMarket()
-        {
-            // Initialize data structure
+        public CardMarket() {
+            // initialize data structure
             this.decksMap = new HashMap<>();
-            for(Color color : Color.values())
-            {
+            for(Color color : Color.values()) {
                 List<LinkedList<DevelopmentCard>> column = new ArrayList<>();
-                for(int i = 0; i < 3; i++)
-                {
-                    column.add(new LinkedList<DevelopmentCard>());
+                for(int i = 0; i < 3; i++) {
+                    column.add(new LinkedList<>());
                 }
                 this.decksMap.put(color,column);
             }
-            // Load Development Cards
+            // load Development Cards
             List<DevelopmentCard> devCards = DevelopmentCard.getDevelopmentCardDeck("src/main/resources/JSON/DevelopmentCard.json");
             Collections.shuffle(devCards);
             for(DevelopmentCard card : devCards)
@@ -155,15 +139,14 @@ public class MarketBoard {
 
         /**
          * Returns the cost of the first DevelopmentCard available with
-         * the specified Color and level; if no card matches
+         * the specified Color and level; if no Card matches
          * the given parameters throws NoSuchDevelopmentCardException.
-         * @param level the level of the required card.
-         * @param color the color of the required card.
-         * @return the ResourcePack representing the cost of the required card.
-         * @throws NoSuchDevelopmentCardException if no card matches the given parameters.
+         * @param level the level of the required Card.
+         * @param color the color of the required Card.
+         * @return the ResourcePack representing the cost of the required Card.
+         * @throws NoSuchDevelopmentCardException if no Card matches the given parameters.
          */
-        public ResourcePack getCost(int level, Color color) throws NoSuchDevelopmentCardException
-        {
+        public ResourcePack getCost(int level, Color color) throws NoSuchDevelopmentCardException {
             LinkedList<DevelopmentCard> stack = this.decksMap.get(color).get(level - 1);
 
             if(stack.isEmpty()) throw new NoSuchDevelopmentCardException();
@@ -179,20 +162,18 @@ public class MarketBoard {
          * @return the required DevelopmentCard.
          * @throws NoSuchDevelopmentCardException if the specified deck is empty.
          */
-        public DevelopmentCard getDevelopmentCard(int level, Color color) throws NoSuchDevelopmentCardException
-        {
+        public DevelopmentCard getDevelopmentCard(int level, Color color) throws NoSuchDevelopmentCardException {
             LinkedList<DevelopmentCard> stack = this.decksMap.get(color).get(level - 1);
             if(stack.isEmpty()) throw new NoSuchDevelopmentCardException();
             else return stack.pollFirst();
         }
 
         /**
-         * Test if no Development Cards of the specified Color are available.
+         * Tests if no Development Cards of the specified Color are available.
          * @param color the Color whose availability is to test.
          * @return true if no more Development Cards of the specified Color are available, false otherwise.
          */
-        public boolean isOutOfStock(Color color)
-        {
+        public boolean isOutOfStock(Color color) {
             for(List<DevelopmentCard> level : this.decksMap.get(color))
                 if(!level.isEmpty()) return false;
             return true;
@@ -207,14 +188,11 @@ public class MarketBoard {
          * @param amount the amount of DevelopmentCards to discard.
          * @return false if the specified column is left empty, true otherwise.
          */
-        public boolean discard(Color color, int amount)
-        {
+        public boolean discard(Color color, int amount) {
             int level = 0;
             List<LinkedList<DevelopmentCard>> column = this.decksMap.get(color);
-            for(int i = 0; i < amount; i++)
-            {
-                if(column.get(level).pollFirst() == null)
-                {
+            for(int i = 0; i < amount; i++) {
+                if(column.get(level).pollFirst() == null) {
                     // It is necessary to remove from a higher level
                     level++;
                     if(level == 3) return false; // The column is empty
@@ -228,27 +206,23 @@ public class MarketBoard {
         }
 
         @Override
-        public String toString()
-        {
-            String res = "{";
-            for(int row = 2; row >= 0; row--)
-            {
-                res = res + "\n\t";
-                for(Color color : Color.values())
-                {
-                    res = res + " " + this.decksMap.get(color).get(row).size();
+        public String toString() {
+            StringBuilder res = new StringBuilder("{");
+            for(int row = 2; row >= 0; row--) {
+                res.append("\n\t");
+                for(Color color : Color.values()) {
+                    res.append(" ").append(this.decksMap.get(color).get(row).size());
                 }
             }
-            res = res + "\n}";
-            return res;
+            res.append("\n}");
+            return res.toString();
         }
     }
 
     /**
      * Constructs a MarketBoard with all the necessary resources and DevelopmentCards.
      */
-    public MarketBoard()
-    {
+    public MarketBoard() {
         this.resourceMarket = new ResourceMarket();
         this.cardMarket = new CardMarket();
     }
@@ -259,8 +233,7 @@ public class MarketBoard {
      * @param row the row to take.
      * @return the ResourcePack of gathered resources.
      */
-    public ResourcePack takeRow(int row)
-    {
+    public ResourcePack takeRow(int row) {
         return this.resourceMarket.getRow(row);
     }
 
@@ -270,8 +243,7 @@ public class MarketBoard {
      * @param column the column to take.
      * @return the ResourcePack of gathered resources.
      */
-    public ResourcePack takeColumn(int column)
-    {
+    public ResourcePack takeColumn(int column) {
         return this.resourceMarket.getColumn(column);
     }
 
@@ -284,8 +256,7 @@ public class MarketBoard {
      * @return the ResourcePack representing the cost of the required card.
      * @throws NoSuchDevelopmentCardException if no card matches the requirement.
      */
-    public ResourcePack getCost(int level, Color color) throws NoSuchDevelopmentCardException
-    {
+    public ResourcePack getCost(int level, Color color) throws NoSuchDevelopmentCardException {
         return this.cardMarket.getCost(level,color);
     }
 
@@ -298,8 +269,7 @@ public class MarketBoard {
      * @return the first available DevelopmentCard with the given color and level.
      * @throws NoSuchDevelopmentCardException if no card matches the requirement.
      */
-    public DevelopmentCard getDevelopmentCard(int level, Color color) throws NoSuchDevelopmentCardException
-    {
+    public DevelopmentCard getDevelopmentCard(int level, Color color) throws NoSuchDevelopmentCardException {
         return this.cardMarket.getDevelopmentCard(level,color);
     }
 
@@ -312,18 +282,15 @@ public class MarketBoard {
      * @param amount the amount of DevelopmentCards to discard.
      * @return false if there are not any DevelopmentCard of the specified color left, true otherwise.
      */
-    public boolean discard(Color color, int amount)
-    {
+    public boolean discard(Color color, int amount) {
         return this.cardMarket.discard(color, amount);
     }
 
-    public String getResourceMarketView()
-    {
+    public String getResourceMarketView() {
         return this.resourceMarket.toString();
     }
 
-    public String getCardMarketView()
-    {
+    public String getCardMarketView() {
         return this.cardMarket.toString();
     }
 }
